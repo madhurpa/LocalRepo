2b28a59d45ab92604d3ace298423dc64d1243f01
madhurpa <madhurpa@cybage.com>
2024-05-01 21:59:30 +0530

    Added incorrect changes

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,7 +1,9 @@
 public class MaximumNumber {
     public static void main(String[] args) {
-        int[] numbers = {10, 5, 8, 20, 15};
+        int[] numbers;
         int max = numbers[0]; // Assuming the first element is the maximum initially
+
+        numbers = {10, 5, 8, 20, 15};
 
         for (int i = 1; i < numbers.length; i++) {
             if (numbers[i] > max) {
